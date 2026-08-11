3d027707d46b922548b23ea5f07e889fad3ffef4
Faqih Ikhtibar <faqihikhtibar@gmail.com>
2026-08-11 10:58:19 +0700

    feat: Logout

--- src/layout/AppSidebar.vue
+++ src/layout/AppSidebar.vue
@@ -1,10 +1,20 @@
 <script setup>
 import { useAuthStore } from '@/stores/auth.store';
+import { Button, Dialog } from 'primevue';
+
 import { ref } from 'vue';
 import { useRoute } from 'vue-router';
 
 const authStore = useAuthStore()
 const { user } = authStore
+
+const logoutDialog = ref (false)
+const handleLogout = async ()=>{
+    await authStore.logout()
+    logoutDialog.value = false
+    router.push({ name: 'login'})
+}
+
 const route =useRoute();
 
 const menuItems =ref([
@@ -50,7 +60,7 @@ const menuItems =ref([
         </div>
         <!-- User Profile-->
         <div class="p-4 border-t border-surface-200">
-            <div class="w-full flex items-center gap-3 p-3 rounded-xl hover:bg-surface-50">
+            <button @click="logoutDialog = true" class="w-full flex items-center gap-3 p-3 rounded-xl hover:bg-surface-50">
                 <div class="w-9 h-9 rounded-full bg-surface-200 flex items-center justify-center overflow-hidden">
                     <i class="pi pi-user text-lg text-surface-600"></i>
                 </div>
@@ -65,8 +75,15 @@ const menuItems =ref([
                 <div class="ml-auto w-8-h8 rounded-lg flex items-center justify-center text-surface-400 group-hover:bg-red-50 group-hover:text--red-600 transition-colors">
                     <i class="pi pi-sign-out text-lg"></i>
                 </div>
-            </div>
+            </button>
         </div>
     </div>
+    <Dialog v-model:visible="logoutDialog" header="Confirm Logout" :modal="true" class="w-100 ">
+        <span class="text-surface-500 block mb-8">Are you sure to Logout</span>
+        <div class="flex justify-end gap-2">
+            <Button type="button" label="Cancel" severity="secondary" @click="logoutDialog= false"/>
+            <Button type="button" label="Logout" severity="danger" @click="handleLogout"/>
+        </div>
+    </Dialog>
      
 </template>
